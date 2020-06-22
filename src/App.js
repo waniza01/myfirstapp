23553c70e748c3397ad9430ce5ba74b1b5d07985
WanizaNaz <waniza.naz0103@gmail.com>
2020-06-22 16:04:46 +0500

    practice function component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Dinner from './dinner.js'
 
 function App() {
   return (
-   <h1>Hello world</h1>
+   <Dinner />
   );
 }
 
---
+++ src/dinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dinner(){
+    return(
+        <div>
+            <h1>Welcome to my app</h1>
+            <h1>Today we are serving chicken biryani and also sweet dishes</h1>
+        </div>
+    );
+}
+export default Dinner;
